6e34078004ef71ba312ced24143b94bbd97998f6
dannyo669 <dannyoleary1@gmail.com>
2015-10-05 10:39:21 +0100

    Users toString using Guava library

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,6 @@
 package models;
+//import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.MoreObjects.toStringHelper;
 
 public class User 
 {
@@ -6,16 +8,25 @@ public class User
   public String lastName;
   public String email;
   public String password;
-
+  
   public User()
   {
   }
-
+  
   public User(String firstName, String lastName, String email, String password)
   {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+  }
+  
+  public String toString()
+  {
+    return toStringHelper(this).addValue(firstName)
+                               .addValue(lastName)
+                               .addValue(password)
+                               .addValue(email)                               
+                               .toString();
   }
 }
